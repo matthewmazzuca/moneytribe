804a14a5be3ab624e30963975717d71b21aea36f
matthewmazzuca <matthew.mazzuca@mail.utoronto.ca>
2016-05-08 03:17:49 -0400

    setting change

--- src/moneytribe/settings.py
+++ src/moneytribe/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%79dgedy2pid+!pvtapdz_ii&k02x7x$3qbj6ueufzyp@cd$0i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
